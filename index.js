ef67ba5eafd107a6380e71a2b3e2ab32c44db8f1
Ankit Bhagat <annonymous007banda@gmail.com>
2024-01-17 11:21:32 +0530

    More controler features added-mute and replay

--- index.js
+++ index.js
@@ -28,29 +28,63 @@ play_pause_btn.addEventListener("click", play_pause=function () {
     }
 });
 
+document.querySelector(".replay-btn").addEventListener("click",()=>{
+  
+  document.querySelector(".progress").style.width = 0 +"px";
+  document.querySelector(".pointer").style.transform = `translateX(0px)`;
+
+  music.pause();
+  clearInterval(intervalId);    
+
+  play_pause_btn.setAttribute("src", "./playback/pause.svg");
+
+  music=new Audio(musicPath[musicNumber]);
+  
+  intervalId = setInterval(progress, 500);
+
+  music.play();
+});
+
+const volumne_btn=document.querySelector(".volume-btn").addEventListener("click",function () {
+    music.muted=!music.muted
+    let path=["./playback/volume.svg","./playback/mute.svg"];    
+    document.querySelector(".volume-btn").setAttribute("src",path[(music.muted)+0]);
+});
+
 const prev=document.querySelector(".prev");
 const next=document.querySelector(".next");
 
-next.addEventListener("click", ()=>{
+next.addEventListener("click", ()=>{  
+  document.querySelector(".progress").style.width = 0 +"px";
+  document.querySelector(".pointer").style.transform = `translateX(-6px)`;
+
   music.pause();
+  
   clearInterval(intervalId);  
-  intervalId = setInterval(progress, 500);
 
-  play_pause_btn.setAttribute("src", "./playback/pause.svg")
-
+  play_pause_btn.setAttribute("src", "./playback/pause.svg");
   musicNumber=(musicNumber+1)%musicPath.length;
+
+  intervalId = setInterval(progress, 500);
+  
   music=new Audio(musicPath[musicNumber]);
   music.play();
 });
 
 prev.addEventListener("click", ()=>{
-  music.pause();
-  clearInterval(intervalId);  
-  intervalId = setInterval(progress, 500);
-  play_pause_btn.setAttribute("src", "./playback/pause.svg")
+  document.querySelector(".progress").style.width = 0 +"px";
+  document.querySelector(".pointer").style.transform = `translateX(-6px)`;
 
+  music.pause();
+
+  clearInterval(intervalId);  
+
+  play_pause_btn.setAttribute("src", "./playback/pause.svg");
   musicNumber=(musicNumber-1)===-1?musicPath.length-1:musicNumber-1;
   music=new Audio(musicPath[musicNumber]);
+
+  intervalId = setInterval(progress, 500);
+
   music.play();
 });
 
@@ -65,7 +99,7 @@ const progress=function() {
   let totalDuration=duration();
   let movement=(parseFloat(document.querySelector(".progress-bar").offsetWidth)/Math.ceil(totalDuration))*Math.ceil(currentTime());
 
-  document.querySelector(".progress").style.width = movement + "px";
+  document.querySelector(".progress").style.width = movement - 6 +"px";
   document.querySelector(".pointer").style.transform = `translateX(${movement-10}px)`;
   // console.log("Here");
 
